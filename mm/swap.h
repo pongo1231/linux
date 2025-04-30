ea4f0bc8f417d07d6d98ef8360b41495d17145f1
Qun-Wei Lin <qun-wei.lin@mediatek.com>
2025-04-30 16:26:41 +0800

    mm: Add Kcompressd for accelerated memory compression
    This patch series introduces a new mechanism called kcompressd to
    improve the efficiency of memory reclaiming in the operating system.
    
    Problem:
      In the current system, the kswapd thread is responsible for both scanning
      the LRU pages and handling memory compression tasks (such as those
      involving ZSWAP/ZRAM, if enabled). This combined responsibility can lead
      to significant performance bottlenecks, especially under high memory
      pressure. The kswapd thread becomes a single point of contention, causing
      delays in memory reclaiming and overall system performance degradation.
    
    Solution:
      Introduced kcompressd to handle asynchronous compression during memory
      reclaim, improving efficiency by offloading compression tasks from
      kswapd. This allows kswapd to focus on its primary task of page reclaim
      without being burdened by the additional overhead of compression.
    
    In our handheld devices, we found that applying this mechanism under high
    memory pressure scenarios can increase the rate of pgsteal_anon per second
    by over 260% compared to the situation with only kswapd. Additionally, we
    observed a reduction of over 50% in page allocation stall occurrences,
    further demonstrating the effectiveness of kcompressd in alleviating memory
    pressure and improving system responsiveness.
    
    Co-developed-by: Barry Song <21cnbao@gmail.com>
    Signed-off-by: Barry Song <21cnbao@gmail.com>
    Signed-off-by: Qun-Wei Lin <qun-wei.lin@mediatek.com>
    Reference: Re: [PATCH 0/2] Improve Zram by separating compression context from kswapd - Barry Song
               https://lore.kernel.org/lkml/20250313093005.13998-1-21cnbao@gmail.com/

--- mm/swap.h
+++ mm/swap.h
@@ -181,6 +181,7 @@ static inline void swap_read_unplug(struct swap_iocb *plug)
 void swap_write_unplug(struct swap_iocb *sio);
 int swap_writeout(struct folio *folio, struct writeback_control *wbc);
 void __swap_writepage(struct folio *folio, struct writeback_control *wbc);
+int kcompressd(void *p);
 
 /* linux/mm/swap_state.c */
 extern struct address_space swap_space __ro_after_init;
@@ -421,6 +422,11 @@ static inline unsigned int folio_swap_flags(struct folio *folio)
 
 static inline int swap_zeromap_batch(swp_entry_t entry, int max_nr,
 		bool *has_zeromap)
+{
+	return 0;
+}
+
+static inline int kcompressd(void *p)
 {
 	return 0;
 }
