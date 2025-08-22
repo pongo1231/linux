8dd1d126acc9be4216f438a23c68bd33e543a83c
pongo1231 <pongo@gopong.dev>
2025-08-22 14:00:52 +0200

    asus

--- drivers/platform/x86/asus-nb-wmi.c
+++ drivers/platform/x86/asus-nb-wmi.c
@@ -146,8 +146,12 @@ static struct quirk_entry quirk_asus_ignore_fan = {
 	.wmi_ignore_fan = true,
 };
 
-static struct quirk_entry quirk_asus_zenbook_duo_kbd = {
-	.ignore_key_wlan = true,
+static struct quirk_entry quirk_asus_wlan_ignore = {
+	.key_wlan_event = ASUS_WMI_KEY_IGNORE,
+};
+
+static struct quirk_entry quirk_asus_wlan_armoury = {
+	.key_wlan_event = ASUS_WMI_KEY_ARMOURY,
 };
 
 static int dmi_matched(const struct dmi_system_id *dmi)
@@ -528,7 +532,16 @@ static const struct dmi_system_id asus_quirks[] = {
 			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
 			DMI_MATCH(DMI_PRODUCT_NAME, "UX8406MA"),
 		},
-		.driver_data = &quirk_asus_zenbook_duo_kbd,
+		.driver_data = &quirk_asus_wlan_ignore,
+	},
+	{
+		.callback = dmi_matched,
+		.ident = "ASUS ROG Z13 2025",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "ROG Flow Z13 GZ302"),
+		},
+		.driver_data = &quirk_asus_wlan_armoury,
 	},
 	{
 		.callback = dmi_matched,
@@ -537,7 +550,7 @@ static const struct dmi_system_id asus_quirks[] = {
 			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
 			DMI_MATCH(DMI_PRODUCT_NAME, "UX8406CA"),
 		},
-		.driver_data = &quirk_asus_zenbook_duo_kbd,
+		.driver_data = &quirk_asus_wlan_ignore,
 	},
 	{},
 };
@@ -636,6 +649,7 @@ static const struct key_entry asus_nb_wmi_keymap[] = {
 	{ KE_IGNORE, 0xCF, },	/* AC mode */
 	{ KE_KEY, 0xFA, { KEY_PROG2 } },           /* Lid flip action */
 	{ KE_KEY, 0xBD, { KEY_PROG2 } },           /* Lid flip action on ROG xflow laptops */
+	{ KE_KEY, ASUS_WMI_KEY_ARMOURY, { KEY_PROG3 } },
 	{ KE_END, 0},
 };
 
@@ -655,11 +669,9 @@ static void asus_nb_wmi_key_filter(struct asus_wmi_driver *asus_wmi, int *code,
 		if (atkbd_reports_vol_keys)
 			*code = ASUS_WMI_KEY_IGNORE;
 		break;
-	case 0x5D: /* Wireless console Toggle */
-	case 0x5E: /* Wireless console Enable */
-	case 0x5F: /* Wireless console Disable */
-		if (quirks->ignore_key_wlan)
-			*code = ASUS_WMI_KEY_IGNORE;
+	case 0x5F: /* Wireless console Disable / Special Key */
+		if (quirks->key_wlan_event)
+			*code = quirks->key_wlan_event;
 		break;
 	}
 }
